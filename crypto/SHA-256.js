097450872d96c4a3c255fc7f50a1c57d7170d069
BradReeves <bradleyaaronreeves@gmail.com>
2019-12-17 09:23:47 -0800

    format update

--- crypto/SHA-256.js
+++ crypto/SHA-256.js
@@ -1,14 +1,14 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js"></script>
 <script type="text/javascript">
-    function genHash() {
-        const ID = '0000';
-        const KEY = '000000000000000000000000000000';
-	var userId = document.getElementById('USERID');
-	var time = new Date().getTime();
+function genHash() {
+    const ID = '0000';
+    const KEY = '000000000000000000000000000000';
+    var userId = document.getElementById('USERID');
+    var time = new Date().getTime();
 
-	var signature = CryptoJS.HmacSHA256(userId.innerText + ID + time, KEY);
-    };
+    var signature = CryptoJS.HmacSHA256(userId.innerText + ID + time, KEY);
+};
 
-    genHash();
+genHash();
 </script>
